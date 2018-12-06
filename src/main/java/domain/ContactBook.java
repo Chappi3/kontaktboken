7b910a711c8b6bdbce3cad9dab7747df1e97d3d3
Tonny <tonny035@hotmail.com>
2018-12-06 14:20:09 +0100

    add comments

--- src/main/java/domain/ContactBook.java
+++ src/main/java/domain/ContactBook.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 
 public class ContactBook implements Serializable {
 
+    // fields
     private ArrayList<Contact> contactBook = new ArrayList<>();
     public static final long serialVersionUID = 748717L;
 
+    // getter
     public ArrayList<Contact> getContactBook() {
         return contactBook;
     }

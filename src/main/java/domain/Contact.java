0aa503afde67494a7710f897f2e82ed27279567f
maks <maxCoder777>
2018-12-06 13:51:58 +0100

    syntax error i Contact "lasttName"->"lastName"

--- src/main/java/domain/Contact.java
+++ src/main/java/domain/Contact.java
@@ -10,9 +10,9 @@ public class Contact implements Serializable {
     public static final long serialVersionUID = 44L;
 
     //Constructor
-    public Contact(String firstName, String lasttName, String phoneNumber, String email) {
+    public Contact(String firstName, String lastName, String phoneNumber, String email) {
         this.firstName = firstName;
-        this.lastName = lasttName;
+        this.lastName = lastName;
         this.phoneNumber = phoneNumber;
         this.email = email;
     }
@@ -45,11 +45,11 @@ public class Contact implements Serializable {
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-    public String getLasttName() {
+    public String getLastName() {
         return lastName;
     }
-    public void setLasttName(String lasttName) {
-        this.lastName = lasttName;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
     public String getPhoneNumber() {
         return phoneNumber;

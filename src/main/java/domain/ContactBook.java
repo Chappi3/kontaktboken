ddce76a6fff085ad8c95d47a83f2929ffbcf215e
Tonny <tonny035@hotmail.com>
2018-12-06 13:54:48 +0100

    Add ArrayList

--- src/main/java/domain/ContactBook.java
+++ src/main/java/domain/ContactBook.java
@@ -1,9 +1,11 @@
 package domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class ContactBook implements Serializable {
 
+    private ArrayList<Contact> contactBook = new ArrayList<>();
     public static final long serialVersionUID = 748717L;
 
 }

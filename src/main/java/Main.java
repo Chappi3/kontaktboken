a589bee9470e6643139e23d323084a7be3ee8ad5
maks <maxCoder777>
2018-12-10 21:18:51 +0100

    Menu class created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,8 @@
+import app.Menu;
 import domain.Contact;
 
 public class Main {
     public static void main(String[] args) {
-//        Contactbook c=new ContactBook()
-//        New Menu(c).start();
-        System.out.println(new Contact("Max", "G" ,"8923734726", "jshdsh@jkcs.com"));
+
     }
 }

89f50a8c4a5bb86d1eae435a201b56d96074750c
maks <maxCoder777>
2018-12-10 21:21:52 +0100

    createMenu() created

--- src/main/java/app/Menu.java
+++ src/main/java/app/Menu.java
@@ -19,4 +19,38 @@ public class Menu {
         System.out.println("_____________________|");
     }
 
+    public String[] createMenu(){
+        System.out.println("\nCreate contact:");
+        while(true){
+            System.out.print("Name: ");
+            input = sc.nextLine();
+            if(Validator.checkName(input)){
+                firstName = input; break;
+            }
+        }
+        while(true){
+            System.out.print("Last name: ");
+            input = sc.nextLine();
+            if(Validator.checkName(input)){
+                lastName = input; break;
+            }
+        }
+        while(true){
+            System.out.print("Phone number: ");
+            input = sc.nextLine();
+            if(Validator.checkPhoneNumber(input)){
+                phoneNumber = input; break;
+            }
+        }
+        while(true){
+            System.out.print("Email: ");
+            input = sc.nextLine();
+            if(Validator.checkEmail(input)){
+                email = input; break;
+            }
+        }
+        String[] data = new String[]{firstName, lastName, phoneNumber, email};
+        return data;
+    }
+
 }

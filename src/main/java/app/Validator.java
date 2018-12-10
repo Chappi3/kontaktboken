820764196615e976274edbe5c074a2b9265179c3
maks <maxCoder777>
2018-12-10 21:24:58 +0100

    Validator -> checkName(), check name

--- src/main/java/app/Validator.java
+++ src/main/java/app/Validator.java
@@ -1,5 +1,13 @@
 package app;
 
 public class Validator {
+    //Check first or last name (text only and start with uppercase, ex. "Daniel")
+    public static boolean checkName(String input){
+        if(input.matches("[A-Z]{1}[a-z]{2,20}")){
+            return true;
+        }
+        return false;
+    }
+
     
 }

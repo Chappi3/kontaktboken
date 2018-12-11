6fbfd4d096fbbdc6b56a163d54abf19f8926e3c4
Ilari Silander <ilarisilander@hotmail.com>
2018-12-11 14:26:44 +0000

    Add Search class

---
+++ src/main/java/app/Search.java
@@ -0,0 +1,35 @@
+package app;
+
+import domain.ContactBook;
+
+public class Search {
+
+    public void searchName(String name, ContactBook contactBook) {
+
+//        for(int i = 0; i < contactBook.getContactBook().size(); i++) {
+//            contactBook.getContactBook().get(i)
+//
+//
+//        }
+
+    }
+
+    public void searchLastName(String lastName, ContactBook contactBook) {
+
+
+
+    }
+
+    public void searchEmail(String email, ContactBook contactBook) {
+
+
+
+    }
+
+    public void searchNumber(String number, ContactBook contactBook) {
+
+
+
+    }
+
+}

28d2a0076d6cc60c3ccae473d308e53de4a8dcf7
Ilari Silander <ilarisilander@hotmail.com>
2018-12-12 10:18:54 +0000

    Add comments to Create and SearchTest

--- src/main/java/app/Create.java
+++ src/main/java/app/Create.java
@@ -5,10 +5,16 @@ import domain.ContactBook;
 public class Create {
 
     public static void createContact(ContactBook contactBook) {
+        //Create a new menu object
         Menu menu = new Menu();
-        String[] data = menu.createMenu();//firstName, lastName, phoneNumber, email
 
+        //We put all the inputs (firstName, lastName, phoneNumber, email) from menu.createMenu() into an Array
+        String[] data = menu.createMenu();
+
+        //Create a new contact and take the inputs from our Array as parameters for the contact
         Contact contact = new Contact(data[0], data[1], data[2],data[3]);
+
+        //Add contact to contactBook
         contactBook.getContactBook().add(contact);
     }
 }
--- src/test/java/app/SearchTest.java
+++ src/test/java/app/SearchTest.java
@@ -8,28 +8,32 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SearchTest {
 
+    //Creates a contact book
     ContactBook contactBook = new ContactBook();
+
+    //Creates a contact with parameters for the test
     Contact contact = new Contact("ilari", "silander","123456", "email@email.se");
 
-    @Test
+    @Test //Checks if the index number 0 is a contact and if it contains (getter) a string "ilari"
     void doesContactBookIndexZeroGetFirstNameContainCorrectName() {
+        //Add the contact into the contactbook
         contactBook.getContactBook().add(contact);
         assertTrue(contactBook.getContactBook().get(0).getFirstName().equals("ilari"), "Should contain ilari");
     }
 
-    @Test
+    @Test //Checks if the index number 0 is a contact and if it contains (getter) a string "silander"
     void doesContactBookIndexZeroGetLastNameContainCorrectLastName() {
         contactBook.getContactBook().add(contact);
         assertTrue(contactBook.getContactBook().get(0).getLastName().equals("silander"), "Should contain silander");
     }
 
-    @Test
+    @Test //Checks if the index number 0 is a contact and if it contains (getter) a string "email@email.se"
     void doesContactBookIndexZeroGetEmailNameContainTheCorrectEmail() {
         contactBook.getContactBook().add(contact);
         assertTrue(contactBook.getContactBook().get(0).getEmail().equals("email@email.se"), "Should contain email@email.se");
     }
 
-    @Test
+    @Test //Checks if the index number 0 is a contact and if it contains (getter) a string "123456"
     void doesContactBookIndexZeroGetNumberContainTheCorrectNumber() {
         contactBook.getContactBook().add(contact);
         assertTrue(contactBook.getContactBook().get(0).getPhoneNumber().equals("123456"), "Should contain 123456");

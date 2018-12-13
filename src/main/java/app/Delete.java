8cc6cac7a27de9a4d3bf5f2c2ffcdbb2b7237d58
Tonny <tonny035@hotmail.com>
2018-12-13 13:03:43 +0100

    add javadoc

--- src/main/java/app/Delete.java
+++ src/main/java/app/Delete.java
@@ -2,8 +2,21 @@ package app;
 
 import domain.ContactBook;
 
+/**
+ * Class for removing a contact from a contactBook
+ *
+ * @author Tonny Frisk
+ * @since 2018-12-12
+ */
+
 public class Delete {
 
+    /**
+     * This method takes a contactBook that we send to deleteMenu.
+     * deleteMenu returns an index that will be removed from contactBook.
+     *
+     * @param contactBook The parameter where we remove a contact from.
+     */
     // for removing a contact from the contactBook
     public static void deleteContact(ContactBook contactBook) {
         Menu menu = new Menu();

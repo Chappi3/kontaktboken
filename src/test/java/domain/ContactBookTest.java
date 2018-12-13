69dd99c2f1472fc8d4dd07dcd48e9829db53fa34
Tonny <tonny035@hotmail.com>
2018-12-13 12:28:55 +0100

    update ContactBookTest

--- src/test/java/domain/ContactBookTest.java
+++ src/test/java/domain/ContactBookTest.java
@@ -10,10 +10,17 @@ class ContactBookTest {
     private Contact contact = new Contact("Tonny","Frisk","123456","test@mail.com");
     private ContactBook contactBook = new ContactBook();
 
-    // Tests if getContactBook returns correctly
+    // Tests if getContactBook returns right size
     @Test
-    void getContactBook() {
+    void doesContactBookGetRightSize() {
         contactBook.getContactBook().add(contact);
-        assertEquals(1, contactBook.getContactBook().size());
+        assertEquals(1, contactBook.getContactBook().size(), "Test failed");
+    }
+
+    // checks if the added contact is the same one that is returned
+    @Test
+    void doesContactBookReturnCorrectContacts() {
+        contactBook.getContactBook().add(contact);
+        assertTrue(contactBook.getContactBook().contains(contact), "Test failed");
     }
 }

237199aecb63396f413fa3b5b232e51a040e133f
hjalmar-arnell <hjalmararnell@gmail.com>
2018-12-17 16:29:12 +0100

    fix for public static void main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,10 +14,8 @@ import java.util.Scanner;
 
 public class Main {
 
-    /**
-     * Start current application
-     */
-    public static void main() {
+
+    public static void main(String[] args) {
         try {
             start();
             System.out.println("Exit...");

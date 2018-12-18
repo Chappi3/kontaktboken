697cacca7945e1bae9dcd6ddbc611d6b95bdd54d
Maksym-Gorbunov <maxsverige@hotmail.com>
2018-12-18 13:08:07 +0100

    testar git konflict maks

--- src/main/java/ShowConflictExample.java
+++ src/main/java/ShowConflictExample.java
@@ -1,5 +1,6 @@
 public class ShowConflictExample {
     public static void main(String[] args) {
+        System.out.println("Line from MAKS branch");
         System.out.println("Line from MASTER branch");
     }
 }
